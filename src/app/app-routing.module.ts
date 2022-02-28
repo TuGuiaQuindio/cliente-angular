4ed049ae90ed285be0ff37d5d4b4a2626d7c88e2
dunas26 <jhonjamesillo@gmail.com>
2022-02-28 09:33:06 -0500

    UPDATE: App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,12 @@ import { ContactoComponent } from './components/contenedores/contacto/contacto.c
 // Se crea array que tendra las rutas
 const routes: Routes = [
   // Cada objeto sera una ruta
-  {path: '' , component: HomeComponent },
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'contacto', component: ContactoComponent}
+  // {path: '' , component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'home', loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule) }
 
 ];
 
