fc93862eb4737e1aad533f49c377c3d5770b9720
dunas26 <jhonjamesillo@gmail.com>
2022-03-13 09:50:41 -0500

    REMOVE: Old test for app running

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('vistasProyectoTaildwind');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vistasProyectoTaildwind app is running!');
-  });
 });
