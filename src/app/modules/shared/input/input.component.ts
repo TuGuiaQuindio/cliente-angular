1f19937e0a3818c7c022e509779292e71b4fb7be
dunas26 <jhonjamesillo@gmail.com>
2022-04-15 22:11:16 -0500

    REFACTOR: Warning message behaviour

--- src/app/modules/shared/input/input.component.ts
+++ src/app/modules/shared/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -9,14 +9,15 @@ export class InputComponent implements OnInit {
 
   constructor() { }
 
+
   @Input() public label = "Default";
   @Input() public type = "text";
-
-  public warningMsg = "";
+  @Input() public warningMsg = "";
 
   ngOnInit(): void {
   }
 
+  @HostBinding('class.warning') 
   public get showWarning() {
     return this.warningMsg.length != 0;
   }
