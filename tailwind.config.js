f428f9d1c4ad2a5114e231eaa0a995d1851bf93f
dunas26 <jhonjamesillo@gmail.com>
2022-04-15 19:59:23 -0500

    UPDATE: Tailwind style configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,32 @@ module.exports = {
   content: ["src/**/*.{html,ts}"],
   theme: {
     extend: {
+      gridTemplateRows: {
+        'card': '7rem 1fr auto',
+      },
+      boxShadow: {
+        'drop-around': ['0 2px 12px 2px rgba(0 0 0 / 0.125)', '0 4px 8px -4px rgba(0 0 0 / 0.15)'],
+      },
       fontFamily: {
         'logo': ['MuseoModerno', 'cursive'],
         'heading': ['Varela Round', 'sans-serif'],
         'normal': ['Cabin', 'sans-serif']
       },
+      outlineWidth: {
+        'card': "6px"
+      },
+      transitionProperty: {
+        'outline': 'outline',
+        'card': 'outline, transform',
+      },
+      scale: {
+        'xs': '1.025',
+      },
       colors: {
         primary: "#FFF",
-        secondary: { 600: "#33BD67", 400: "#BBF19A" },
+        secondary: { 600: "#33BD67", 500: "#6FCE67", 400: "#BBF19A", },
         accent: "#5CE1C9",
-        dark: { 600: "#4C4451", 400: "#9F9F9F" },
+        dark: { 600: "#4C4451", 500: "#838383", 400: "#9F9F9F", },
       }
     },
   },
