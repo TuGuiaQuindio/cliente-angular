1f2b3b1c7b6844c0b187b7a2e6d3e58ff615b3ba
dunas26 <jhonjamesillo@gmail.com>
2022-04-18 10:03:52 -0500

    REMOVE: Unused routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterComponent } from './components/contenedores/register/register.component';
-import { ContactoComponent } from './components/contenedores/contacto/contacto.component';
-
 // Se crea array que tendra las rutas
 const routes: Routes = [
   // Cada objeto sera una ruta
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'contacto', component: ContactoComponent },
   { path: 'home', loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule) },
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
   { path: 'choose', loadChildren: () => import('./modules/choose-register/choose-register.module').then(m => m.ChooseRegisterModule) },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { RegisterComponent } from './components/contenedores/register/register.component';
-import { ContactoComponent } from './components/contenedores/contacto/contacto.component';
 import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    ContactoComponent
   ],
   imports: [
     BrowserModule,
