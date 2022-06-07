5507b08ce2a9f915b9b3bbe52bd2124ba3455035
dunas26 <jhonjamesillo@gmail.com>
2022-06-07 13:09:37 -0500

    FIX: Unused imports on module solver

--- src/app/modules/dashboard/services/module-solver.service.ts
+++ src/app/modules/dashboard/services/module-solver.service.ts
@@ -1,6 +1,5 @@
-import { Component, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { USER_ROLE } from 'src/app/constants';
-import { AuthService } from 'src/app/core/services/auth.service';
 import { CompanyExtraFormComponent } from '../components/company-extra-form/company-extra-form.component';
 import { GuideExtraFormComponent } from '../components/guide-extra-form/guide-extra-form.component';
 import { DashboardServicesModule } from './dashboard-services.module';
