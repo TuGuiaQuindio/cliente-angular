4bfd78ba46a7ed4d1244d6c4096eabb2cd803945
dunas26 <jhonjamesillo@gmail.com>
2022-06-07 07:01:23 -0500

    UPDATE: Interceptor header and routing events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { Injector, NgModule } from '@angular/core';
+import { NgModule, OnDestroy } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavigationEnd, Router, Scroll } from '@angular/router';
+import { filter, Observable, Subject, takeUntil } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { THEME_KEY } from './constants';
 import { CoreModule } from './core/core.module';
 import { FormServicesModule } from './modules/form-services/form-services.module';
 
@@ -22,6 +25,27 @@ import { SharedModule } from './modules/shared/shared.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor() { }
+export class AppModule implements OnDestroy {
+
+  private lifecycleSubj = new Subject<string>();
+  private get destroy$(): Observable<string> {
+    return this.lifecycleSubj.pipe(filter(x => x === 'destroy'));
+  }
+
+  constructor(router: Router) {
+    router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$),
+      )
+      .subscribe({
+        next: (event) => {
+          document.body.dataset["theme"] = localStorage.getItem(THEME_KEY) ?? "default";
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.lifecycleSubj.next('destroy');
+  }
 }
--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -8,12 +8,18 @@ import {
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export const CUSTOM_URL_HEADER = "X-Custom-Ur";
+
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
   constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.headers.has(CUSTOM_URL_HEADER)) {
+      const headers = request.headers.delete(CUSTOM_URL_HEADER);
+      return next.handle(request.clone({ headers }));
+    }
     const url = environment.url_api + request.url;
     const cloneReq = request.clone({ url });
     return next.handle(cloneReq);
