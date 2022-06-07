4fd4e7529d0598ada8c7c38aa47fb0fc8889715e
dunas26 <jhonjamesillo@gmail.com>
2022-06-07 13:53:51 -0500

    NEW: Panel settings

---
+++ src/app/modules/settings/components/panel-settings/panel-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-settings',
+  templateUrl: './panel-settings.component.html',
+  styleUrls: ['./panel-settings.component.scss']
+})
+export class PanelSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 import { SharedModule } from '../shared/shared.module';
+import { PanelSettingsComponent } from './components/panel-settings/panel-settings.component';
 
 
 @NgModule({
   declarations: [
-    SettingsComponent
+    SettingsComponent,
+    PanelSettingsComponent
   ],
   imports: [
     CommonModule,
