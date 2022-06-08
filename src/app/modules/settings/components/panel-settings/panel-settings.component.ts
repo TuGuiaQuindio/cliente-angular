5673f3ce4f893e18854ce0c6c12a6a54f8902f86
dunas26 <jhonjamesillo@gmail.com>
2022-06-07 22:44:11 -0500

    UPDATE: Panel settings section

--- src/app/modules/settings/components/panel-settings/panel-settings.component.ts
+++ src/app/modules/settings/components/panel-settings/panel-settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+export type PanelSectionDefinition = { title: string, buttons: PanelButtonDefinition[] }
+export type PanelButtonDefinition = { icon: string, label: string, url: string }
+
 @Component({
   selector: 'app-panel-settings',
   templateUrl: './panel-settings.component.html',
@@ -8,6 +11,22 @@ import { Component, OnInit } from '@angular/core';
 export class PanelSettingsComponent implements OnInit {
 
   constructor() { }
+  public buttonSection: PanelSectionDefinition[] = [
+    { title: "Mi perfil", buttons: 
+      [
+        { icon: "bx-user", label: "información personal", url: "/settings" },
+        { icon: "bx-lock-alt", label: "seguridad", url: "/settings" },
+        { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
+      ]
+    },
+    { title: "Privacidad", buttons: 
+      [
+        { icon: "bx-user", label: "información personal", url: "/settings" },
+        { icon: "bx-lock-alt", label: "seguridad", url: "/settings" },
+        { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
+      ]
+    },
+  ]
 
   ngOnInit(): void {
   }
