a1d0d9967f523432c018f303dd335a960f611d48
dunas26 <jhonjamesillo@gmail.com>
2022-06-09 09:13:56 -0500

    UPDATE: Settings modules and page view

--- src/app/modules/settings/routes/security/security.component.ts
+++ src/app/modules/settings/routes/security/security.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+export type SettingSectionDefinition = { title: string, inputs: SettingInputDefinition[], formGroup: FormGroup }
+export type SettingInputDefinition = { name: string, description: string, formControlName: string, inputType: string }
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
   styleUrls: ['./security.component.scss']
 })
-export class SecurityComponent implements OnInit {
+export class SecurityComponent {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  public passwordChange = this.fb.group({
+    password: ['', Validators.required],
+    newPassword: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  })
 
-  ngOnInit(): void {
-  }
+  public sections: SettingSectionDefinition[] = [
+    {
+      title: "Cambio de contraseña", formGroup: this.passwordChange, inputs: [
+        { name: "contraseña", description: "¿cuál es tu contraseña actual?", formControlName: "password", inputType: "password" },
+        { name: "nueva contraseña", description: "escribe tu nueva contraseña aquí", formControlName: "password", inputType: "password" },
+        { name: "confirmar contraseña", description: "confirma tu nueva contraseña para estar seguros", formControlName: "password", inputType: "password" },
+      ]
+    },
+  ]
 
 }
