c385138e3ab1271c20bbd41f5d342abd55411363
dunas26 <jhonjamesillo@gmail.com>
2022-06-09 20:47:26 -0500

    UPDATE: Security component refactor

--- src/app/modules/settings/routes/security/security.component.ts
+++ src/app/modules/settings/routes/security/security.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidatorMatchDirective } from 'src/app/modules/signup/directives/validator-match.directive';
+import { SettingSectionDefinition } from '../../components/panel-builder/panel-builder.component';
 
-export type SettingSectionDefinition = { title: string, inputs: SettingInputDefinition[], formGroup: FormGroup }
-export type SettingInputDefinition = { name: string, description: string, formControlName: string, inputType: string }
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
@@ -10,21 +10,27 @@ export type SettingInputDefinition = { name: string, description: string, formCo
 })
 export class SecurityComponent {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
   public passwordChange = this.fb.group({
     password: ['', Validators.required],
     newPassword: ['', Validators.required],
     confirmPassword: ['', Validators.required],
-  })
+  }, { validators: [ValidatorMatchDirective.matchWith('newPassword', 'confirmPassword')] })
+
+  public dataForm = this.fb.group({
+    passwordChange: this.passwordChange
+  });
 
   public sections: SettingSectionDefinition[] = [
     {
       title: "Cambio de contraseña", formGroup: this.passwordChange, inputs: [
         { name: "contraseña", description: "¿cuál es tu contraseña actual?", formControlName: "password", inputType: "password" },
-        { name: "nueva contraseña", description: "escribe tu nueva contraseña aquí", formControlName: "password", inputType: "password" },
-        { name: "confirmar contraseña", description: "confirma tu nueva contraseña para estar seguros", formControlName: "password", inputType: "password" },
+        { name: "nueva contraseña", description: "escribe tu nueva contraseña aquí", formControlName: "newPassword", inputType: "password" },
+        { name: "confirmar contraseña", description: "confirma tu nueva contraseña para estar seguros", formControlName: "confirmPassword", inputType: "password" },
       ]
     },
   ]
 
+  public onSubmit() {
+  }
 }
