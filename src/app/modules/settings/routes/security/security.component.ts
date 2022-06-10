04ead87389869e532a47a4b4ebb4d6bd5afa2372
dunas26 <jhonjamesillo@gmail.com>
2022-06-10 16:45:09 -0500

    UPDATE: Security component

--- src/app/modules/settings/routes/security/security.component.ts
+++ src/app/modules/settings/routes/security/security.component.ts
@@ -1,35 +1,38 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ValidatorMatchDirective } from 'src/app/modules/signup/directives/validator-match.directive';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { SettingSectionDefinition } from '../../components/panel-builder/panel-builder.component';
+import { ConfigurationDefinition, ConfigurationSolverService } from '../../services/configuration-solver.service';
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
   styleUrls: ['./security.component.scss']
 })
-export class SecurityComponent {
+export class SecurityComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) {}
-  public passwordChange = this.fb.group({
-    password: ['', Validators.required],
-    newPassword: ['', Validators.required],
-    confirmPassword: ['', Validators.required],
-  }, { validators: [ValidatorMatchDirective.matchWith('newPassword', 'confirmPassword')] })
-
-  public dataForm = this.fb.group({
-    passwordChange: this.passwordChange
+  private configurationSubj = new BehaviorSubject<ConfigurationDefinition>({
+    dataForm: new FormGroup({}),
+    sections: [],
   });
 
-  public sections: SettingSectionDefinition[] = [
-    {
-      title: "Cambio de contraseña", formGroup: this.passwordChange, inputs: [
-        { name: "contraseña", description: "¿cuál es tu contraseña actual?", formControlName: "password", inputType: "password" },
-        { name: "nueva contraseña", description: "escribe tu nueva contraseña aquí", formControlName: "newPassword", inputType: "password" },
-        { name: "confirmar contraseña", description: "confirma tu nueva contraseña para estar seguros", formControlName: "confirmPassword", inputType: "password" },
-      ]
-    },
-  ]
+  public get configurationState$() {
+    return this.configurationSubj.asObservable();
+  }
+
+  constructor(private fb: FormBuilder, private configurationSolverSrv: ConfigurationSolverService) { }
+  ngOnInit(): void {
+    const configuration = this.configurationSolverSrv.getSectionConfiguration("1", "security");
+    if (!configuration) return;
+    const { sections, dataForm } = configuration;
+    console.warn("Section", sections)
+    this.configurationSubj.next({
+      sections, dataForm
+    })
+  }
+
+  public dataForm!: FormGroup;
+  public sections!: SettingSectionDefinition[];
 
   public onSubmit() {
   }
