8d0754b943402acdd5d64c92ab9da2f280310a1e
dunas26 <jhonjamesillo@gmail.com>
2022-06-11 22:48:57 -0500

    UPDATE: Panel button selected based on URL change

--- src/app/modules/settings/components/panel-settings/panel-settings.component.ts
+++ src/app/modules/settings/components/panel-settings/panel-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { concatMap, filter, map, of, Subject, take, takeUntil } from 'rxjs';
 
 export type PanelSectionDefinition = { title: string, buttons: PanelButtonDefinition[] }
 export type PanelButtonDefinition = { icon: string, label: string, url: string }
@@ -8,26 +10,65 @@ export type PanelButtonDefinition = { icon: string, label: string, url: string }
   templateUrl: './panel-settings.component.html',
   styleUrls: ['./panel-settings.component.scss']
 })
-export class PanelSettingsComponent implements OnInit {
+export class PanelSettingsComponent {
 
-  constructor() { }
+  constructor(router: Router) {
+    this.solveSelectedButton(router.url);
+  }
+
+  public solveSelectedButton(url: string) {
+    const urlParts = url.split("/").filter(part => part.length != 0);
+    if (urlParts.length < 2) return;
+
+    const section = urlParts[1];
+    type ButtonPart = { parts: string[], button: PanelButtonDefinition }
+    type ButtonAndSegment = { lastSegment: string, button: PanelButtonDefinition }
+
+    of(this.buttonSection)
+      .pipe(
+        concatMap(sections => sections),
+        concatMap(section => section.buttons),
+        map(button => {
+          return {
+            parts: button.url.split("/").filter(part => part.length != 0),
+            button: button
+          } as ButtonPart
+        }),
+        filter(({ parts }) => parts.length >= 2),
+        map(({ button, parts }) => {
+          return {
+            lastSegment: parts[parts.length - 1],
+            button
+          } as ButtonAndSegment
+        }),
+        filter(({ lastSegment }) => lastSegment === section),
+        take(1)
+      ).subscribe({
+        next: ({ button }) => {
+          this.currentButton = button;
+        }
+      })
+  }
+
   public buttonSection: PanelSectionDefinition[] = [
-    { title: "Mi perfil", buttons: 
-      [
-        { icon: "bx-user", label: "información personal", url: "/settings/information" },
-        { icon: "bx-lock-alt", label: "seguridad", url: "/settings/security" },
-        { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
-      ]
+    {
+      title: "Mi perfil", buttons:
+        [
+          { icon: "bx-user", label: "información personal", url: "/settings/information" },
+          { icon: "bx-lock-alt", label: "seguridad", url: "/settings/security" },
+          { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
+        ]
     },
-    { title: "Privacidad", buttons: 
-      [
-        { icon: "bx-user", label: "información personal", url: "/settings" },
-        { icon: "bx-lock-alt", label: "seguridad", url: "/settings" },
-        { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
-      ]
+    {
+      title: "Privacidad", buttons:
+        [
+          { icon: "bx-user", label: "información personal", url: "/settings" },
+          { icon: "bx-lock-alt", label: "seguridad", url: "/settings" },
+          { icon: "bx-book-bookmark", label: "mis certificaciones", url: "/settings" },
+        ]
     },
   ]
-  
+
   private currentButton?: PanelButtonDefinition;
 
   public isSelected(button: PanelButtonDefinition) {
@@ -37,8 +78,4 @@ export class PanelSettingsComponent implements OnInit {
   public selectButton(button: PanelButtonDefinition) {
     this.currentButton = button;
   }
-
-  ngOnInit(): void {
-  }
-
 }
