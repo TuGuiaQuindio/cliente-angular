ff8d32977112f4917e7a3aebea39ea159a491b48
dunas26 <jhonjamesillo@gmail.com>
2022-06-19 01:13:49 -0500

    UPDATE: Language input value functions

--- src/app/modules/language-form/components/language-input/language-input.component.ts
+++ src/app/modules/language-form/components/language-input/language-input.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Optional, Output, Self, ViewChild } from '@angular/core';
+import { NgControl } from '@angular/forms';
 import { map, last, concatMap, scan, Subject, filter, BehaviorSubject } from 'rxjs';
+import { InputValueAccessor } from 'src/app/classes/input-value-accessor';
 import { Language } from 'src/app/mock/data';
 import { SelectOption } from 'src/app/modules/shared/components/select/select.component';
+import { StepsComponent } from 'src/app/modules/shared/components/steps/steps.component';
 import { LanguageApiService } from '../../services/language-api.service';
 
 export type CertificationLevel = {
   name: string,
-  description: string
+  description: string,
+  level: number,
 }
 export type SelectState = {
   options: SelectOption[],
   selected: number,
   certificate: CertificationLevel
 }
+export type LanguageInputState = {
+  language: Language,
+  certification: CertificationLevel & { level: number }
+}
 @Component({
   selector: 'app-language-input',
   templateUrl: './language-input.component.html',
   styleUrls: ['./language-input.component.scss']
 })
-export class LanguageInputComponent implements OnInit, OnDestroy {
+export class LanguageInputComponent extends InputValueAccessor implements OnInit, OnDestroy {
 
-  constructor(private languageSrv: LanguageApiService) { }
+  constructor(@Self() @Optional() ngControl: NgControl, private languageSrv: LanguageApiService) {
+    super(ngControl);
+  }
+  @Output() public languageSelected = new EventEmitter<LanguageInputState>();
+  @ViewChild(StepsComponent) public steps!: StepsComponent;
 
   public certificationLevels: CertificationLevel[] = [
-    { name: "A1", description: "Principiante" },
-    { name: "A2", description: "Elemental" },
-    { name: "B1", description: "Intermedio" },
-    { name: "B2", description: "Intermedio Superior" },
-    { name: "C1", description: "Avanzado" },
-    { name: "C2", description: "Competente" },
+    { name: "A1", level: 0, description: "Principiante" },
+    { name: "A2", level: 1, description: "Elemental" },
+    { name: "B1", level: 2, description: "Intermedio" },
+    { name: "B2", level: 3, description: "Intermedio Superior" },
+    { name: "C1", level: 4, description: "Avanzado" },
+    { name: "C2", level: 5, description: "Competente" },
   ]
   public currentLanguage?: Language;
+  public currentCertification: CertificationLevel = this.certificationLevels[0];
   public lifecycle = new Subject<string>();
   public optionStateSubj = new BehaviorSubject<SelectState>({
     options: [],
@@ -69,30 +82,67 @@ export class LanguageInputComponent implements OnInit, OnDestroy {
         }
       })
   }
-  
+
   public ngOnDestroy() {
     this.lifecycle.next('destroy');
     this.lifecycle.complete();
   }
 
-  public languageSelected(select: SelectOption) {
+  public reset() {
+    this.currentLanguage = undefined;
+    this.currentCertification = this.certificationLevels[0];
+    this.steps.setSelected(0);
+    this.setNgControlValue();
+    const value = this.optionStateSubj.value;
+    this.optionStateSubj.next({
+      ...value,
+      selected: 0, 
+      certificate: this.currentCertification,
+    })
+  }
+
+  private setNgControlValue(languageName?: string, certificationLevel: number = 0) {
+    if (this.ngControl) this.ngControl.control!.setValue({ name: languageName, level: certificationLevel })
+  }
+
+  public onLanguageSelected(select: SelectOption) {
     this.currentLanguage = select.value;
     const value = this.optionStateSubj.value;
     const { options } = value;
     const languageIdx = options.findIndex(el => select == el);
     if (languageIdx == -1) return;
+    const certificationLevel = this.certificationLevels.findIndex((el) => el === this.currentCertification);
     this.optionStateSubj.next({
       ...value,
       selected: languageIdx
     })
+    this.languageSelected.emit({
+      language: select.value,
+      certification: {
+        ...this.currentCertification!,
+        level: certificationLevel,
+      }
+    });
+    this.setNgControlValue(this.currentLanguage?.name, certificationLevel ?? 0);
   }
 
   public onCertificationLevelSelected(idx: number) {
     const certification = this.certificationLevels[idx];
+    this.currentCertification = certification;
     const value = this.optionStateSubj.value;
     this.optionStateSubj.next({
       ...value,
       certificate: certification
     });
+    if (this.currentLanguage == null) return;
+    this.currentCertification
+    this.languageSelected.emit({
+      language: this.currentLanguage,
+      certification: {
+        ...certification,
+        level: idx,
+      }
+    })
+    this.setNgControlValue(this.currentLanguage.name, idx)
   }
 }
