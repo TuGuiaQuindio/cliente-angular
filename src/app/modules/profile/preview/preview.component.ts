9cf5528a4d15155d547e853dfed712590804ee15
andres06 <andressierrarojas7@gmail.com>
2022-06-23 07:40:50 -0500

    Create ngFor

--- src/app/modules/profile/preview/preview.component.ts
+++ src/app/modules/profile/preview/preview.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { of, map } from 'rxjs';
 
+export type Language = { name: string, level: number }
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
@@ -8,6 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class PreviewComponent implements OnInit {
 
   constructor() { }
+  @Input() public title = '';
+  @Input() public languages: Language[] = [
+    { name: "Ingles", level: 4.5 },
+    { name: "Ruso", level: 3.1 },
+    { name: "Ingles", level: 4.5 },
+    { name: "Ruso", level: 3.1 },
+    { name: "Ingles", level: 4.5 },
+    { name: "Ruso", level: 3.1 },
+  ];
+  @Input() public badge = [];
+  @Input() public additions = [];
+  @Input() public aboutMe = '';
+
+  public get languages$() {
+    return of(this.languages)
+      .pipe(
+        map((languages) => languages.sort((a, b) => a.level < b.level ? 1 : -1).slice(0, 3)),
+      )
+  }
 
   ngOnInit(): void {
   }
