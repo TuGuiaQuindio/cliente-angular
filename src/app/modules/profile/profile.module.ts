6d2f2c76919edbb1d42150d82f580e991148905b
dunas26 <jhonjamesillo@gmail.com>
2022-06-27 10:24:53 -0500

    NEW: Guide card component

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PreviewComponent } from './preview/preview.component';
 import { SharedModule } from '../shared/shared.module';
 import { LanguageFormModule } from '../language-form/language-form.module';
+import { GuideCardComponent } from './components/guide-card/guide-card.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
     PreviewComponent,
+    GuideCardComponent,
     CardItemComponent,
   ],
   imports: [
