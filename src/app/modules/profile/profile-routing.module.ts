d1e32dab0bfd6a9baee8de67ae68a2fad3d235a6
dunas26 <jhonjamesillo@gmail.com>
2022-06-27 17:25:47 -0500

    UPDATE: Guide list implementation

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileGuideComponent } from './components/profile-guide/profile-guide.component';
+import { GuideListComponent } from './components/guide-list/guide-list.component';
 
 const routes: Routes = [
-  { path: 'guide/:id', component: ProfileGuideComponent }
+  { path: 'guides', component: GuideListComponent },
+  { path: 'guide/:id', component: ProfileGuideComponent },
 ]
 
 @NgModule({
