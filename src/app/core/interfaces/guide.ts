704718567f7b4522e02341e26adf751f1f055566
dunas26 <jhonjamesillo@gmail.com>
2022-06-27 15:38:33 -0500

    NEW: Guide interface

---
+++ src/app/core/interfaces/guide.ts
@@ -0,0 +1,10 @@
+import { AdditionalInformation } from "src/app/modules/profile/components/extra-info-card/extra-info-card.component";
+import { Language } from "src/app/modules/profile/preview/preview.component";
+
+export interface Guide {
+  firstName: string;
+  lastName: string;
+  aboutMe: string;
+  languages: Language[];
+  additionalInformation: AdditionalInformation;
+}
