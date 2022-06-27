d848120e4c323e13f22fab31e70ed01dc7af541e
dunas26 <jhonjamesillo@gmail.com>
2022-06-27 16:59:59 -0500

    NEW: Truncate pipe

--- src/app/modules/pipes/pipes.module.ts
+++ src/app/modules/pipes/pipes.module.ts
@@ -4,12 +4,13 @@ import { HasCountPipe } from './pipes/has-count.pipe';
 import { ExistPipe } from './pipes/exist.pipe';
 import { HasEntriesPipe } from './pipes/has-entries.pipe';
 import { SplitPipe } from './pipes/split.pipe';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
-  declarations: [HasCountPipe, ExistPipe, HasEntriesPipe, SplitPipe],
+  declarations: [HasCountPipe, ExistPipe, HasEntriesPipe, SplitPipe, TruncatePipe],
   imports: [
     CommonModule
   ],
-  exports: [HasCountPipe, ExistPipe, HasEntriesPipe, SplitPipe]
+  exports: [HasCountPipe, ExistPipe, HasEntriesPipe, SplitPipe, TruncatePipe]
 })
 export class PipesModule { }
