953f80c845033af617e7ff0f55ddac6a832eb9a7
andres06 <andressierrarojas7@gmail.com>
2022-06-29 06:28:06 -0500

    MODIFIED: enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // url_api: 'https://backend-env.up.railway.app'//NAME SERVICE BACKEND
-  url_api: 'http://localhost:3100'//NAME SERVICE BACKEND
+  url_api: 'https://backend-env.up.railway.app'//NAME SERVICE BACKEND
+  // url_api: 'http://localhost:3100'//NAME SERVICE BACKEND
 };
