c1092f501fc2e64f54d6399a4533adbd4162fe02
dunas26 <jhonjamesillo@gmail.com>
2022-06-29 06:55:58 -0500

    UPDATE: Guide list lifecycle subject

--- src/app/modules/profile/components/guide-list/guide-list.component.ts
+++ src/app/modules/profile/components/guide-list/guide-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject, map, mergeMap, Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, filter, map, mergeMap, Observable, Subject, takeUntil } from 'rxjs';
 import { Guide } from 'src/app/core/interfaces/guide';
 import { FilterSidebarComponent } from 'src/app/modules/filter/components/filter-sidebar/filter-sidebar.component';
 import { GuideDataService } from '../../services/guide-data.service';
@@ -9,11 +9,12 @@ import { GuideDataService } from '../../services/guide-data.service';
   templateUrl: './guide-list.component.html',
   styleUrls: ['./guide-list.component.scss']
 })
-export class GuideListComponent implements OnInit {
+export class GuideListComponent implements OnInit, OnDestroy {
 
   constructor(private dataSrv: GuideDataService) { }
   @ViewChild(FilterSidebarComponent) public set hostFilterSidebar(filterSidebar: FilterSidebarComponent) {
     filterSidebar.stateChange.pipe(
+      takeUntil(this.destroy$),
       mergeMap((state) => this.dataSrv.getAllGuides().pipe(
         map((guides) => {
           return guides.filter(guide => {
@@ -36,6 +37,14 @@ export class GuideListComponent implements OnInit {
     });
   }
   private guidesSubj = new BehaviorSubject<Guide[]>([]);
+  private lifecycle = new Subject<string>();
+
+  private get destroy$() {
+    return this.lifecycle.asObservable()
+      .pipe(
+        filter(state => state == 'destroy')
+      );
+  }
 
   public get guides$(): Observable<Guide[]> {
     return this.guidesSubj.asObservable();
@@ -51,6 +60,11 @@ export class GuideListComponent implements OnInit {
         this.guidesSubj.next(guides);
       }
     })
+  }
+
+  ngOnDestroy() {
+    this.lifecycle.next('destroy');
+    this.lifecycle.complete();
   }
 
 }
