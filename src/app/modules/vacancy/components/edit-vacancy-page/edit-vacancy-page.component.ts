b5100c50fffb14edada99eaf2980874c33a2fb4e
dunas26 <jhonjamesillo@gmail.com>
2022-07-01 02:10:00 -0500

    UPDATE: Edit vacancy form injection

--- src/app/modules/vacancy/components/edit-vacancy-page/edit-vacancy-page.component.ts
+++ src/app/modules/vacancy/components/edit-vacancy-page/edit-vacancy-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormInjectorService } from 'src/app/core/services/form-injector.service';
+import { VacancyDataService } from '../../services/vacancy-data.service';
 import { VacancyFormComponent } from '../../vacancy-form/vacancy-form.component';
 
 @Component({
@@ -7,19 +9,38 @@ import { VacancyFormComponent } from '../../vacancy-form/vacancy-form.component'
   templateUrl: './edit-vacancy-page.component.html',
   styleUrls: ['./edit-vacancy-page.component.scss']
 })
-export class EditVacancyPageComponent implements OnInit {
+export class EditVacancyPageComponent implements AfterViewInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router, private vacancySrv: VacancyDataService, private formInject: FormInjectorService) {
     const { id } = route.snapshot.params;
     console.warn(id);
-    if (!id){
+    if (!id) {
       this.router.navigateByUrl('/vacancy');
       return;
     }
+    this.vacancyId = id;
   }
+  public vacancyId = "";
   @ViewChild(VacancyFormComponent) public vacancyForm!: VacancyFormComponent;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.vacancySrv.getVacancyById(this.vacancyId).subscribe({
+      next: (vacancy) => {
+        if (!vacancy) return;
+        const { title, id, languages, salaryMax, salaryMin, availability, vacancyCount, description } = vacancy;
+        setTimeout(() => {
+          this.formInject.start(this.vacancyForm.form).inject({
+            title: title,
+            description,
+            personnel: vacancyCount,
+            minSalary: salaryMin,
+            maxSalary: salaryMax,
+            availability: availability,
+            languages
+          })
+        })
+      }
+    });
   }
 
   public onSubmit() {
